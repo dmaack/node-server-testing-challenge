ab890f9a0bdd4190738653e90aa6d4c2cee2585e
Dominique Maack <dominiquemaack@Dominiques-MBP.domain>
2019-11-14 15:34:24 -0700

    added chefs-model

--- chefs/chefs-model.js
+++ chefs/chefs-model.js
@@ -1,5 +1,34 @@
 const db = require('../data/dbConfig')
 
 module.exports = {
-    
+    find,
+    findBy,
+    findById,
+    add,
+    remove
+}
+
+function find() {
+    return db('chefs')
+}
+
+function findBy(filter) {
+    return db('chefs')
+    .where(filter)
+}
+
+function findById(id) {
+    return db('chefs')
+    .where({ id })
+}
+
+function add(newChef) {
+    return db('chefs')
+    .insert(newChef)
+}
+
+function remove(id) {
+    return db('chefs')
+    .where({ id })
+    .delete();
 }
